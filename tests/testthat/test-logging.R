69d11f2423e49d9bc859702463377a398511f193
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2026-07-15 14:40:23 +0200

    update log testing

--- tests/testthat/test-logging.R
+++ tests/testthat/test-logging.R
@@ -1,7 +1,7 @@
 context("Logging")
 
 test_that("logger can be created and deleted", {
-  logger <- log4r::create.logger()
+  logger <- create_logger()
   token <- "test"
 
   expect_false(exists(paste0("logger", token), envir = wilson.globals))
@@ -13,15 +13,20 @@ test_that("logger can be created and deleted", {
 
 test_that("message can be logged", {
   logfile <- tempfile()
-  logger <- log4r::create.logger(logfile = logfile, level = "DEBUG")
+  logger <- create_logger(logfile = logfile, level = "DEBUG", name = "test_log")
   token <- "test_log"
 
   set_logger(logger = logger, token = token)
-  expect_false(file.exists(logfile))
+  # lgr's AppenderFile creates the (empty) file on construction, so check for
+  # content rather than existence.
+  expect_equal(file.size(logfile), 0)
   log_message("test message", level = "DEBUG", token = token)
-  expect_true(file.exists(logfile))
+  expect_gt(file.size(logfile), 0)
+  expect_true(any(grepl("test message", readLines(logfile))))
+
   file.remove(logfile)
   set_logger(logger = NULL, token = token)
+  # with no logger registered for the token, nothing is written / no file created
   log_message("test message", level = "DEBUG", token = token)
   expect_false(file.exists(logfile))
 })
