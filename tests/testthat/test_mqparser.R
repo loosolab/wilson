d1aee18cb7fab4e2516c6b0a6a45ef0c148ebd6f
Schultheis <hschult@kerckhoff.mpg.de>
2026-07-27 13:28:20 +0200

    use tempfile for outputs

--- tests/testthat/test_mqparser.R
+++ tests/testthat/test_mqparser.R
@@ -1,25 +1,29 @@
 context("MaxQuant parser")
 
 test_that("all needed input parameteres are given", {
+
   expect_error(parse_MaxQuant(), "The proteinGroups file was not given")
-  expect_error(parse_MaxQuant(proteinGroups_in = "./path/path/"), "The summary file was not given")
-  expect_error(parse_MaxQuant(proteinGroups_in = "./path/path/", summary_in = "./path/path/"),
+  expect_error(parse_MaxQuant(proteinGroups_in = "./invalid/path/"), "The summary file was not given")
+  expect_error(parse_MaxQuant(proteinGroups_in = "./invalid/path/", summary_in = "./invalid/path/"),
                                       "The output file was not given")
-  expect_error(parse_MaxQuant(proteinGroups_in = "./path/path/", summary_in = "./path/path/",
-                                      outfile = "./path/path/"), "The output_reduced file was not given")
+  expect_error(parse_MaxQuant(proteinGroups_in = "./invalid/path/", summary_in = "./invalid/path/",
+                                      outfile = "./invalid/path/"), "The output_reduced file was not given")
 })
 
 test_that("mq_parser", {
 
+  file_full <- tempfile(fileext = "_full.clarion")
+  file_reduced <- tempfile(fileext = "_reduced.clarion")
+
   expect_error(parse_MaxQuant(proteinGroups_in = "proteinGroups_test.txt", summary_in = "summary_test_2.txt",
-                                      outfile = "./out", outfile_reduced = "./outres" ),
+                                      outfile = file_full, outfile_reduced = file_reduced ),
                "wrong format on summary file: column \'Experiment\' misssing")
   expect_true(parse_MaxQuant(proteinGroups_in = "proteinGroups_test.txt", summary_in = "summary_test.txt",
-                                      outfile = "./out", outfile_reduced = "./outres", config = "success_config.json"))
+                                      outfile = file_full, outfile_reduced = file_reduced, config = "success_config.json"))
   expect_error(parse_MaxQuant(proteinGroups_in = "proteinGroups_test.txt", summary_in = "summary_test.txt",
-                                      outfile = "./out", outfile_reduced = "./outres", config = "" ),
+                                      outfile = file_full, outfile_reduced = file_reduced, config = "" ),
                "Could not read config file")
   expect_error(parse_MaxQuant(proteinGroups_in = "proteinGroups_test.txt", summary_in = "summary_test.txt",
-                                      outfile = "./out", outfile_reduced = "./outres", config = "fail_config.json" ),
+                                      outfile = file_full, outfile_reduced = file_reduced, config = "fail_config.json" ),
                "reduced_list is missing in config file")
 })
